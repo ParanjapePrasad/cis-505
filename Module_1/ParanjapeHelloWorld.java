fef574be655a052b24de1d9eb9fb2536e9ce178f
Prasad <pparanjape@my365.bellevue.edu>
2023-09-01 02:27:07 -0400

    Assignment 1.2 GitHub Setup and helloworld file

---
+++ Module_1/ParanjapeHelloWorld.java
@@ -0,0 +1,17 @@
+/*
+ * Liang, Y.D. (2019).  Introduction to Java Programming and Data Structures: Comprehensive Version (12th ed.).  Pearson Education, Inc.
+ * Payne, D., (2021).  CSD 405 Intermediate Java Programming.  Bellevue University, all rights reserved.
+ * Paranjape, Prasad. (2023).  CIS 505 Intermediate Java Programming. Bellevue University.
+ */
+
+package Module_1;
+
+public class ParanjapeHelloWorld {
+    public static void main(String[] args) {
+        String firstName = "Prasad";
+        String lastName = "Paranjape";
+
+        System.out.printf("Hello World from %s %s", firstName, lastName);
+
+    } // end main
+} // end ParanjapeHelloWorld
