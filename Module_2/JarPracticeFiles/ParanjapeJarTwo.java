8d1e3be1622c3a13fc81d22524c3c6b5f18f7d02
Prasad <pparanjape@my365.bellevue.edu>
2023-09-05 03:06:07 -0400

    Adding Module 2 Jar and source files

---
+++ Module_2/JarPracticeFiles/ParanjapeJarTwo.java
@@ -0,0 +1,28 @@
+/*
+ * Liang, Y.D. (2019).  Introduction to Java Programming and Data Structures: Comprehensive Version (12th ed.).  Pearson Education, Inc.
+ * Payne, D. (2021).  CSD 405 Intermediate Java Programming.  Bellevue University, all rights reserved.
+ * Paranjape, Prasad. (2023).  CIS 505 Intermediate Java Programming. Bellevue University.
+ */
+
+package Module_2.JarPracticeFiles;
+
+public class ParanjapeJarTwo {
+
+    /*
+     * Method with two arguments which prints the Hello World with first name and
+     * last name
+     *
+     * @param firstName String
+     *
+     * @param lastName String
+     *
+     */
+    public static void hello(String firstName, String lastname) {
+        System.out.printf(String.format("Hello World from %s %s Jar Two", firstName, lastname));
+    } // end hello
+
+    public static void main(String[] args) {
+        hello("Prasad", "Paranjape");
+    } // end main
+
+} // end ParanjapeJarTwo
